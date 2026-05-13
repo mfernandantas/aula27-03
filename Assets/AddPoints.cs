6ef4cf8d4bc225081a5fe6a8809a912406b9c971
Usagi-Gb <mfernandantas@gmail.com>
2026-05-13 10:42:00 -0300

    pf pou e musica

--- Assets/AddPoints.cs
+++ Assets/AddPoints.cs
@@ -8,6 +8,7 @@ public class AddPoints : MonoBehaviour
     public AudioSource source;
     public LixoSpawnerController lixoSpawnerController;
     private void OnCollisionEnter(Collision collision) {
+        
         // Optional: Check if the entering object is the Player
         if (collision.gameObject.CompareTag("Lixo")) {
             Destroy(collision.gameObject);
